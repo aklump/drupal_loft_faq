3d02ce3a0c278a62b37bf735ba4f0be5dc45c653
aklump <sourcecode@intheloftstudios.com>
2012-06-28 19:42:44 -0700

    fixing small issues

--- loft_faq-list.tpl.php
+++ loft_faq-list.tpl.php
@@ -6,13 +6,14 @@
  * $anchor - the name attribute for the anchor tag; correlates with the toc
  * $category - the santized category name
  * $items - an array of renderable arrays holding the items
+ * $attributes - array
  *
  * @ingroup loft_faq Frequently Asked Questions
  * @{
  */
 ?>
 <a name="<?php print $anchor ?>"></a>
-<div class="loft-faq-category-wrapper <?php print $anchor ?>">
+<div <?php print drupal_attributes($wrapper_attributes) ?>>
   <h2 class="view-title loft-faq-category"><?php print $category ?></h2>
   <dl class="loft-faq-category">
     <?php foreach ($items as $item) : ?>
